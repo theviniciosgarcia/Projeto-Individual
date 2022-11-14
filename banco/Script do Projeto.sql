5f4fbb5b39b1d6d304d5523ea8bbee897998d271
theviniciosgarcia <vinicios.fagundes@sptech.school>
2022-11-14 04:27:42 -0300

    atualizando o script do banco

--- banco/Script do Projeto.sql
+++ banco/Script do Projeto.sql
@@ -19,5 +19,30 @@ descricao varchar(45),
 fkUsuario int, foreign key (fkUsuario) references usuario(idUsuario)
 );
 
+-- Quiz: fazer as validações do time que torçe e responde umas perguntas, com isso sai o resultado tipo de torcedor, 
+-- e juntamente isso o numero de sua camisa.
+
+
 select * from usuario;
+select * from comentario;
+
+select count(tipo_torcedor) as louco,(select count(tipo_torcedor) 
+as fanatico from usuario where tipo_torcedor = 'fanatico') 
+as fanatico, (select count(tipo_torcedor) 
+as comum from usuario where tipo_torcedor = 'comum') 
+as comum from usuario where tipo_torcedor = 'louco';
+
+select count(clube) as corinthians, (select count(clube) 
+ as santos from usuario where clube = 'santos')
+ as santos, (select count(clube) 
+ as sao_paulo from usuario where clube = 'sao_paulo')
+ as sao_paulo, (select count(clube) 
+ as sem_mundial from usuario where clube = 'sem_mundial') 
+ as sem_mundial, (select count(clube) 
+ as outro from usuario where clube = 'outro') 
+ as outro from usuario where clube = 'corinthians';
+
+
+
+
 
